a79a9051082baf87c928ac40bb14e73ee48c0f51
ipek270199 <icansuinanc@gmail.com>
2020-12-14 21:58:32 +0300

    Initial commit

---
+++ app/src/main/java/edu/atilim/ise308/inanc/ipekcansu/project1/Movies.kt
@@ -0,0 +1,12 @@
+package edu.atilim.ise308.inanc.ipekcansu.project1
+
+import com.google.android.material.appbar.AppBarLayout
+import java.lang.NullPointerException
+
+class Movies {
+    var name: String? = null
+    var description: String? = null
+    var view: Int? = null
+    var isWatched: Boolean = false
+    var actor : String? = null
+}
---
+++ app/src/main/java/edu/atilim/ise308/inanc/ipekcansu/project1/NewMovie.kt
@@ -0,0 +1,9 @@
+package edu.atilim.ise308.inanc.ipekcansu.project1
+
+import androidx.fragment.app.DialogFragment
+import androidx.fragment.app.Fragment
+
+class NewMovie : Fragment() {
+
+
+}
